a18cf0744438aafa7446bdf05f8266fbf2de9c4c
Dario Campagna <campagna@esteco.com>
2023-10-17 14:47:09 +0200

    Initial commit

---
+++ src/main/java/sdm/hello/Main.java
@@ -0,0 +1,13 @@
+package sdm.hello;
+
+public class Main {
+
+    public static void main(String... args) {
+        if (args.length == 0) {
+            System.out.println("Hello stranger!");
+        } else {
+            System.out.println("Hello " + args[0] + "!");
+        }
+    }
+    
+}
